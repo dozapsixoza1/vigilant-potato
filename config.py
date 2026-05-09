1efae257b0fabb851879e4345c817c2b610d1c54
dozapsixoza1 <166742425+dozapsixoza1@users.noreply.github.com>
2026-05-09 22:04:41 +0400

    Update config.py

--- config.py
+++ config.py
@@ -6,15 +6,12 @@ OWNER_ID = 7950038145
 SUBSCRIBE_CHAT_ID = -1003844270710  # !!! СЮДА ВСТАВИТЬ РЕАЛЬНЫЙ ID !!!
 SUBSCRIBE_LINK = "https://t.me/+6x1CHb3JxP5kZjU6"
 
-# База данных Neon (вставил твою)
+# База данных Neon
 DB_URL = "postgresql+asyncpg://neondb_owner:npg_Wpbx9jtKPl6y@ep-lucky-butterfly-apy8s59f-pooler.c-7.us-east-1.aws.neon.tech/neondb"
 
-# Подключение к Redis (локальный)
-REDIS_URL = "redis://localhost:6379"
-
-# Тарифы: ключ – days, значение: (цена USDT, description)
+# Тарифы
 PLANS = {
     7: (10, "7 дней"),
     30: (25, "30 дней"),
-    9999: (100, "Навсегда")   # 9999 условно
+    9999: (100, "Навсегда")
 }
